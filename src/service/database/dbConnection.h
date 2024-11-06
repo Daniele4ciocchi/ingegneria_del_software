6e5e4c31c92dc8a67b04f028edc39aac9282b6a1
daniele4ciocchi <danielequattro45@gmail.com>
2024-11-06 15:06:08 +0100

    first commit

---
+++ src/service/database/dbConnection.h
@@ -0,0 +1,42 @@
+#ifndef pgsql_h
+#define pgsql_h
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#include <postgresql/libpq-fe.h>  // libreria libpq, utilizzata per la connessione al db Postgres
+// per farlo funzionare ricordate di installare questa libreria.
+// il comando e':  sudo apt-get install libpq-dev
+// potrebbe funzionare anche cosi: #include <libpq-fe.h>
+
+
+
+class DbConnection {
+
+private:
+ 	    
+  PGconn *dbConnection; // dbconnection contiene il puntatore alla connessione al database
+  // tramite dbconnection è possibile accedere alle informazioni sulla connessione e utilizzare le funzioni fornite dalla libreria libpq per interagire con il database
+
+public:
+
+  //Costruttore
+  DbConnection(const char *hostname,const char *port,const char *username,const char *password,const char *dbname);
+
+  // usata per eseguire Query che non ritornano dati. INSERT, UPDATE e DELETE
+  PGresult* RunActionQuery(char *query);
+
+  // usata per eseguire Query che ritornano dati. SELECT
+  PGresult* RunDataQuery(char *query);
+
+  // runQuery e' la funzione principale per l'esecuzione delle query, chiama, a seconda del valore di moreValue, RunActionQuery o RunDataQuery
+  PGresult* RunQuery(char* query, bool moreValue);
+
+  //Funzione che ci disconnette dal database
+  void disconnectFromDatabase();
+  
+};
+
+
+#endif 
